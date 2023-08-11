90b87ccdc05ddc6cf43ab84f8baabdfed862c738
Anthony <bondolo90@gmail.com>
2023-08-11 07:06:47 -0700

    Commit 3

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This is a package initializer
+"""
+
+from models.engine.file_storage import FileStorage
+
+
+storage = Filestorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 common attributes/methods for other classes"""
 
 import models #importing classes from the models module
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
 class BaseModel:
@@ -18,23 +18,43 @@ class BaseModel:
             **kwargs: key/value pairs representing attributes.
         """
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key != '__class__':
-                    setattr(self, key, value)
-        else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            if len(kwargs) != 0:
+                for key, value, in kwargs.items():
+                    if key == 'created_at' or key == 'updated_at':
+                        self.__dict__[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    else:
+                        self.__dict__[key] = value
+            else:
+                models.storage.new(self)
 
     def __str__(self):
+        """
+        This method returns a formatted string containing the class name,
+        the instance's ID, and its attribute dictionary.
+
+        Returns:
+        str: A string representation of the object.
+        """
+
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """
+        Update 'updated_at' with the current datetime.
+        """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
+        """
+        This method creates a dictionary representation of the object's
+        attributes e.g class name, creation timestamp, & the current timestamp.
+
+        Returns: dict: A dictionary containing object attributes.
+        """
+
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
