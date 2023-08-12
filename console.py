82fa37fd1fc82a7208b72e6a025b6d0d743e1087
Carokil1 <chucholimosr@gmail.com>
2023-08-12 13:37:16 -0700

    commit 13

--- console.py
+++ console.py
@@ -7,14 +7,14 @@ This defines the HBNBCommand class for the command interpreter
 import cmd
 import json
 import shlex
-#from models.engine.file_storage import FileStorage
-#from models.base_model import BaseModel
-#from models.user import User
-#from models.place import Place
-#from models.state import State
-#from models.city import City
-#from models.amenity import Amenity
-#from models.review import Review
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """
